b0c9087b5a39cbe1bc69023c71673de6cc6fb35d
kris <ksowatta@uci.edu>
2023-12-31 23:31:57 -0800

    Completed Day 4 Part 2

--- day4.py
+++ day4.py
@@ -18,3 +18,24 @@ with open('day4.txt', 'r') as f:
 print(total)
 
 # Part 2
+numcards = 0
+with open('day4.txt', 'r') as f:
+    for line in f:
+        numcards += 1
+cardcount = {}
+for i in range(numcards):
+    cardcount[str(i)] = 1
+with open('day4.txt', 'r') as f:
+    for i, line in enumerate(f):
+        line = line.strip()
+        seps = line.split(':')
+        numbers = seps[1].split('|')
+        winning_nums = numbers[0].split(' ')
+        card_nums = numbers[1].split(' ')
+        matches = 0
+        for number in card_nums:
+            if number != '' and number in winning_nums:
+                matches += 1
+        for j in range(matches):
+            cardcount[str(i+j+1)] += cardcount.get(str(i))
+    print(sum(cardcount.values()))
